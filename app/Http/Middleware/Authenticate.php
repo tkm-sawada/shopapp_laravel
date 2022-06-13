50212a5514f874ea761a76ad1eff97ba39e1fbee
tkm-sawada <da.tkm3@gmail.com>
2022-06-13 22:46:26 +0900

    01_Environment

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -6,6 +6,10 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    protected $user_route = 'user.login';
+    protected $owner_route = 'owner.login';
+    protected $admin_route = 'admin.login';
+    
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -15,7 +19,17 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            // 各権限の画面に未ログインでアクセスした場合の処理
+            if(Route::is('owner.*')){
+                // ownerログイン画面へリダイレクト
+                return route($this->owner_route);
+            }elseif(Route::is('admin.*')){
+                // adminログイン画面へリダイレクト
+                return route($this->admin_route);
+            }else{
+                // user画面へリダイレクト
+                return route($this->user_route);
+            }
         }
     }
 }
